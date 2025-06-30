bd05ffb2c2d8c29ffdde2dd851d67009992fce7d
Teja3376 <kcteja62892@gmail.com>
2025-06-30 22:31:28 +0530

    /...

--- vite-project/src/controllers/userController.js
+++ vite-project/src/controllers/userController.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const getName = async (email) => {
-  return axios.get(`http://localhost:5000/get-name?email=${email}`);
+  return axios.get(`https://otp-backend-production.up.railway.app/get-name?email=${email}`);
 };
